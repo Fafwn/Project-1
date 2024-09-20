4258b74c59b465216b2725b1bd5a99189c05fb6e
Fafwn <kaiking02@icloud.com>
2024-09-20 18:39:38 +0100

    00.01.00 - Went absolutely crazy on Map.py, working on implementing a surrounding map to combine individual sections

--- Code/Main.py
+++ Code/Main.py
@@ -1,5 +1,34 @@
-from Imports import Branch
-from Imports import LogConfig
+"""from Imports import Branch
+from Imports import LogConfig"""
 
-LogConfig.run()
-Branch.run()
+
+class CPU:
+    maxReg = 4
+    maxMem = 256
+
+    def __init__(self):
+        self.registers = [0 for _ in range(self.maxReg)]
+        self.memory = [0 for _ in range(self.maxMem)]
+        self.pc = 0
+        self.ir = 0
+        self.state = True
+
+    def fetch(self):
+        self.ir = self.memory[self.pc]
+        self.ir += 1
+
+    def decode(self):
+        pass
+
+    def execute(self):
+        pass
+
+    def run(self):
+        while self.state:
+            self.fetch()
+            self.decode()
+            self.execute()
+
+
+if __name__ == "__main__":
+    instance = CPU()
